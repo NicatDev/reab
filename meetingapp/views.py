db61b76ac01a4f94bbf3aa381e73a2683d77db12
unknown <nicat254memmedov@gmail.com>
2023-10-16 14:14:58 +0400

    22

--- meetingapp/views.py
+++ meetingapp/views.py
@@ -91,7 +91,7 @@ def home(request):
     if meetings.filter(date__gte=datetime.now()).exists():
         nearest_meeting = meetings.filter(date__gte=datetime.now()).order_by('date')[0].date
     
-        print(datetime.now())
+        print(meetings.filter(date__gte=datetime.now()).order_by('date')[0])
         you = 'late'
     else:
         
