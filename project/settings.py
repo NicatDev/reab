3ad00f5a8dfa34f299f10821b84dec35bfc32e63
unknown <nicat254memmedov@gmail.com>
2023-10-17 15:07:52 +0400

    22

--- project/settings.py
+++ project/settings.py
@@ -143,9 +143,9 @@ ORTAWARE_CLASSES = (
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
